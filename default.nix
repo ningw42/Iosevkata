87c7c8184a619e93ac09455768bedf9fd83c98f9
Ning Wang <unidentifiedme26@gmail.com>
2023-12-03 19:21:50 +0800

    remove redundant build plans in default.nix and flake.nix

--- default.nix
+++ default.nix
@@ -1,74 +1,10 @@
 { pkgs ? import <nixpkgs> { system = builtins.currentSystem; } }:
+
 let
   version = "27.3.5";
   hash = "sha256-dqXr/MVOuEmAMueaRWsnzY9MabhnyBRtLR9IDVLN79I=";
   npmDepsHash = "sha256-bux8aFBP1Pi5pAQY1jkNTqD2Ny2j+QQs+QRaXWJj6xg=";
-  privateBuildPlan = ''
-    # Built with https://typeof.net/Iosevka/customizer
-
-    # A `PragmataPro` styled `Iosevka` variant with my tweaks.
-    # 1. **A fixed spacing, no ligature.** I once liked ligature, but it's distracting.
-    # 2. **A higher underscore.** To make underscore-connected characters feels connected, like `Menlo`.
-    # 3. **A lower hex asterisk.** To place it at the center of the line, like `Menlo`.
-    # 4. **An oval-dotted zero.** `PragmataPro`'s diamond shaped zero is too sharp for me.
-    # 5. **A few decorations mimicking `mononoki`.** For 'B', 'D', 'P' and 'R'.
-
-    # Reference:
-    # The metric-override subsection is copied from Pragmasevka and Iosvmata
-
-    [buildPlans.iosevkata]
-    family = "Iosevkata"
-    spacing = "fixed"
-    serifs = "sans"
-    no-cv-ss = true
-    export-glyph-names = false
-
-    [buildPlans.iosevkata.variants]
-    inherits = "ss08"
-
-    [buildPlans.iosevkata.variants.design]
-    underscore = "above-baseline"
-    asterisk = "hex-low"
-    zero = "oval-dotted"
-    capital-b = "standard-unilateral-serifed"
-    capital-d = "more-rounded-unilateral-serifed"
-    capital-p = "closed-motion-serifed"
-    capital-r = "curly-top-left-serifed"
-
-    [buildPlans.iosevkata.widths.normal]
-    shape = 500         # Unit Width, measured in 1/1000 em.
-    menu  = 5           # Width grade for the font's names.
-    css   = "normal"    # "font-stretch' property of webfont CSS.
-
-    [buildPlans.iosevkata.metric-override]
-    leading = 1100      # a smaller line height. built-in line height, default is 1250.
-    xHeight = 550       # a taller 'x'. height of 'x', default is 520.
-
-    [buildPlans.iosevkata.weights.light]
-    shape = 300
-    menu  = 300
-    css   = 300
-
-    [buildPlans.iosevkata.weights.regular]
-    shape = 400
-    menu  = 400
-    css   = 400
-
-    [buildPlans.iosevkata.weights.medium]
-    shape = 500
-    menu  = 500
-    css   = 500
-
-    [buildPlans.iosevkata.weights.semibold]
-    shape = 600
-    menu  = 600
-    css   = 600
-
-    [buildPlans.iosevkata.weights.bold]
-    shape = 700
-    menu  = 700
-    css   = 700
-  '';
+  privateBuildPlan = builtins.readFile ./private-build-plans.toml;
 in
 pkgs.buildNpmPackage rec {
   inherit version privateBuildPlan npmDepsHash;
@@ -109,7 +45,7 @@ pkgs.buildNpmPackage rec {
     mkdir -p "$dist/Iosevkata"
 
     # copy built fonts
-    cp -r "dist/iosevkata/ttf" "$dist/Iosevkata"
+    cp -r "dist/iosevkata/ttf/" "$dist/Iosevkata"
 
     # add built fonts to build artifact
     cd "$dist/Iosevkata"
