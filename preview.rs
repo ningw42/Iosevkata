104fb2384e666613fb49c00e00a2da915eaf24a0
Ning Wang <unidentifiedme26@gmail.com>
2024-01-18 20:26:14 +0800

    update preview.png

---
+++ preview.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // Lowercase letters
+    let lowercase_letters = "abcdefghijklmnopqrstuvwxyz";
+    println!("Lowercase Letters: {}", lowercase_letters);
+
+    // Uppercase letters
+    let uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    println!("Uppercase Letters: {}", uppercase_letters);
+
+    // Numbers
+    let numbers = "0123456789";
+    println!("Numbers: {}", numbers);
+
+    // Common symbols
+    let symbols = "!@#$%^&*()-+=}{[]|\\:;'\"<>,./?`~_";
+    println!("Common Symbols: {}", symbols);
+}
+
