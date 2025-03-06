40742653dde03f6733d924fc2bd739f7c2c30186
Ning Wang <unidentifiedme26@gmail.com>
2025-03-06 21:15:31 +0800

    disable preview image's round corner

--- generate_preview.sh
+++ generate_preview.sh
@@ -1,4 +1,4 @@
 #!/run/current-system/sw/bin/bash
 
 # silicon cannot use external font yet, so we have to install the fonts before generating a preview.
-silicon ./preview.rs --output ./preview.png --language rust --theme ~/.config/bat/themes/catppuccin-frappe.tmTheme --pad-horiz 0 --pad-vert 0 --background '#fff0' --font "Iosevkata=48" --no-window-controls
+silicon ./preview.rs --output ./preview.png --language rust --theme ~/.config/bat/themes/catppuccin-frappe.tmTheme --pad-horiz 0 --pad-vert 0 --background '#fff0' --font "Iosevkata=48" --no-window-controls --no-round-corner
