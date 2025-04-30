80cc20d2c9cfc38c3985467ee15acf0ace4f9c0f
Ning Wang <unidentifiedme26@gmail.com>
2025-04-30 20:32:04 +0800

    init preview theme with catppuccin-frappe

--- generate_preview.sh
+++ generate_preview.sh
@@ -1,4 +1,4 @@
 #!/run/current-system/sw/bin/bash
 
 # silicon cannot use external font yet, so we have to install the fonts before generating a preview.
-silicon ./previews/rust.rs --output ./previews/rust.png --language rust --theme ~/.config/bat/themes/catppuccin-frappe.tmTheme --pad-horiz 0 --pad-vert 0 --background '#fff0' --font "Iosevkata=48" --no-window-controls --no-round-corner
+silicon ./previews/rust.rs --output ./previews/rust.png --language rust --theme ./previews/themes/Catppuccin\ Frappe.tmTheme --pad-horiz 0 --pad-vert 0 --background '#fff0' --font "Iosevkata=48" --no-window-controls --no-round-corner
