e6d6969ed42f54da57643aa0c80f7c1ff821fb2d
Ning Wang <unidentifiedme26@gmail.com>
2025-05-01 12:03:02 +0800

    also generate preview after a new release

--- generate_preview.sh
+++ generate_preview.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # silicon cannot use external font yet, so we have to install the fonts before generating a preview.
-silicon ./previews/rust.rs --output ./previews/rust.png --language rust --theme ./previews/themes/Catppuccin\ Frappe.tmTheme --pad-horiz 0 --pad-vert 0 --background '#fff0' --font "Iosevkata=48" --no-window-controls --no-round-corner
+silicon ./preview/sources/rust.rs --output ./preview/images/rust.png --language rust --theme ./preview/themes/Catppuccin\ Frappe.tmTheme --pad-horiz 0 --pad-vert 0 --background '#fff0' --font "Iosevkata=48" --no-window-controls --no-round-corner
